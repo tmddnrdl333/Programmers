69b47314cf74de28ea903f88a2bc0e8486243947
SeungNong <62299471+tmddnrdl333@users.noreply.github.com>
2022-04-03 21:22:20 +0900

    Add files via upload

---
+++ lv1_키패드_누르기.java
@@ -0,0 +1,12 @@
+package ps_Programmers;
+
+public class lv1_키패드_누르기 {
+	public static void main(String[] args) {
+		System.out.println(solution(new int[] { 1, 3, 4, 5 }, "right"));
+	}
+
+	public static String solution(int[] numbers, String hand) {
+		String answer = "";
+		return answer;
+	}
+}
