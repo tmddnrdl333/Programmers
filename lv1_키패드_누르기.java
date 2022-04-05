e893e92ad323000dbc2747485184fd4d88191157
SeungNong <62299471+tmddnrdl333@users.noreply.github.com>
2022-04-05 16:37:39 +0900

    Update lv1_키패드_누르기.java

--- lv1_키패드_누르기.java
+++ lv1_키패드_누르기.java
@@ -1,6 +1,6 @@
 package ps_Programmers;
 
-public class lv1_키패드_누르기_ing {
+public class lv1_키패드_누르기 {
 	public static void main(String[] args) {
 		System.out.println(solution(new int[] { 1, 3, 4, 5 }, "right"));
 	}
